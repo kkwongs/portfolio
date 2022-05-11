e9cdf6812a9145e3740b369c00a7f0615683dfd0
kkwongs <kihyun5741@gmail.com>
2022-05-11 18:29:39 +0900

    프로젝트 다이얼로그 창 분리

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ window.addEventListener(
 
 const swiper = new Swiper('.swiper', {
   allowTouchMove: false,
+  loop: true,
 
   pagination: {
     el: '.swiper-pagination',
@@ -33,59 +34,27 @@ const swiper = new Swiper('.swiper', {
 
 const projectEl = document.querySelector('.projects');
 const dialogEl = document.createElement('div');
+
 dialogEl.classList.add('dialog', 'hidden');
-dialogEl.innerHTML = `
-<div class="inner">
-  <span class="cancel btn">닫기</span>
-  <div class="title">포트폴리오 웹 사이트</div>
-  <hr />
-  <ul class="info">
-    <li>
-      <div class="title">
-        <span class="material-icons">link</span>URL
-      </div>
-      <a
-        href="https://kkwongs.github.io/portfolio/"
-        target="_blank"
-        class="description"
-        >https://kkwongs.github.io/portfolio/</a
-      >
-    </li>
-    <li>
-      <div class="title">
-        <span class="material-icons">summarize</span>Summary
-      </div>
-      <div class="description">
-        포트폴리오 용도로 제작한 웹사이트입니다.<br /><br />
-        애니메이션 라이브러리인 GSAP을 통해 특정 위치에서 자연스럽게
-        TOP버튼이 나타나고 사라지도록 하였습니다. 그리고 특정 위치를
-        알기위해 Window 인터페이스의 scrollY 속성을 사용했는데 스크롤할
-        때마다 계속해서 호출되는 것을 방지하기 위해 Lodash 라이브러리의
-        _.throttle 메소드를 사용하였습니다. Projects section을 구현하기
-        위해 Swiper 라이브러리를 사용하여 하나의 프로젝트씩 나올 수
-        있도록 슬라이드를 구현하였습니다.
-      </div>
-    </li>
-    <li>
-      <div class="title">
-        <span class="material-icons">construction</span>Technology
-      </div>
-      <div class="description">
-        Frontend: HTML, CSS, JavaScript<br />
-        Deployment: GitHub Pages
-      </div>
-    </li>
-  </ul>
-</div>
-`;
-projectEl.appendChild(dialogEl);
 
-const detailEl = document.querySelector('.projects .btn');
-const cancelEl = document.querySelector('.projects .cancel');
+function fetchProject(project) {
+  fetch(project).then(function (res) {
+    res.text().then(function (text) {
+      gsap.to(dialogEl, 0.2, {
+        opacity: 1,
+        display: 'block',
+      });
+      dialogEl.innerHTML = text;
+      projectEl.appendChild(dialogEl);
 
-detailEl.addEventListener('click', () => {
-  dialogEl.classList.remove('hidden');
-});
-cancelEl.addEventListener('click', () => {
-  dialogEl.classList.add('hidden');
-});
+      const cancelEl = dialogEl.querySelector('.cancel');
+
+      cancelEl.addEventListener('click', () => {
+        gsap.to(dialogEl, 0.2, {
+          opacity: 0,
+          display: 'none',
+        });
+      });
+    });
+  });
+}
